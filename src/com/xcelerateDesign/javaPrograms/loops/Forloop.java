4f7b479e24d51796e00666ca90262d856cda090f
to-jinalmodi <to.jinalmodi@gmail.com>
2024-03-06 21:43:07 -0500

    For loop

---
+++ src/com/xcelerateDesign/javaPrograms/loops/Forloop.java
@@ -0,0 +1,19 @@
+package com.xcelerateDesign.javaPrograms.loops;
+
+/**
+ * Java for loop is divided into various parts as mentioned below
+ *   -Initialization Expression
+ *   -Test Expression
+ *   -Update Expression
+ */
+
+public class Forloop {
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i <= 100; i++) {
+            System.out.print(i + " ");
+        }
+
+    }
+}
